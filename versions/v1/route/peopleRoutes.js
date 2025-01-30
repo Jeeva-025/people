750e5975dd2994bfcfa427ce4e022feb3b4562c2
JSPV220403 <162592820+JSPV220403@users.noreply.github.com>
2025-01-30 18:32:06 +0530

    Update peopleRoutes.js

--- versions/v1/route/peopleRoutes.js
+++ versions/v1/route/peopleRoutes.js
@@ -1,11 +1,14 @@
 // routes/peopleRoutes.js
 const express = require('express');
-const multer= require("../utils/storage");
+const upload= require("../utils/storage");
 const router = express.Router();
+const validateMiddleware= require("../middleware/validation.middleware")
+const validate= require("../validation/validation")
+
 const peopleController = require('../controller/peopleController');
 
 // Route for getting all people
 router.get('/', peopleController.getAllPeople);
-router.post('/insert',peopleController.insertPeople);
+router.post('/insert',validateMiddleware.validateBody(validate.validatePeopleInsert()),upload.single("imagename"),peopleController.insertPeople);
 
 module.exports = router;
